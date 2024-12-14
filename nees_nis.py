cb72be8dcc5f12b2c95cbc5e6331ff5c10758ec3
AegisKnight <metalicaknightmare@gmail.com>
2024-12-13 18:53:30 -0800

    NEES Test code

--- nees_nis.py
+++ nees_nis.py
@@ -43,6 +43,7 @@ def NEES_Chi2_Test(NEES_array, num_states, num_runs, alpha):
     #
     # Outputs
     # NEES_avg: the normalized estimation error squared averaged over runs for each timestep (kx1)
+    # res: get chi statistic (res[0]) and p-value (res[1])
     # 
     # Comments
     # Run this after the monte carlo sim
@@ -72,4 +73,66 @@ def NEES_Chi2_Test(NEES_array, num_states, num_runs, alpha):
     
     return NEES_avg
     
+def NIS(y_real, y_sim, S_k):
     
+    # p: number of measurements recorded
+    #
+    # Inputs
+    # y_real: the measurement vector from real data (px1)
+    # y_sim: the innovation vector post KF update, also know as H matrix times x estimated post prediction (px1)
+    # S_k: the innovation error covariance, also known as HPH.T + R (easily get it from the KF) (pxp)
+    #
+    # Outputs
+    # NIS: the normalized innovation squared (1x1)
+    # 
+    # Comments
+    # Run this at every k time and store NIS into an array, one column array per monte carlo run
+    
+    innov_error = y_real - y_sim
+    NIS = innov_error.T @ S_k @ innov_error
+    
+    return NIS
+
+def NIS_Chi2_Test(NIS_array, num_meas, num_runs, alpha):
+    
+    # k: number of timesteps
+    # r: number of monte carlo runs
+    # p: number of measurements recorded
+    #
+    # Inputs
+    # NIS_array: the normalized innovation squared per k timestep per monte carlo run (kxr)
+    # num_meas: p (number of measurements recorded)
+    # num_runs: r (number of monte carlo runs)
+    # alpha: confidence interval
+    #
+    # Outputs
+    # NIS_avg: the normalized innovation squared averaged over runs for each timestep (kx1)
+    # res: get chi statistic (res[0]) and p-value (res[1]) 
+    # 
+    # Comments
+    # Run this after the monte carlo sim
+    
+    k = np.shape(NIS_array)[0]
+    
+    NIS_avg = np.average(NIS_array, axis=1)
+    NIS_exp = num_meas * np.ones(np.shape(NIS_avg))
+    r1 = chi2.ppf(alpha/2, df=num_runs*num_meas)/num_runs
+    r2 = chi2.ppf(1 - (alpha/2), df=num_runs*num_meas)/num_runs
+    
+    res = stats.chisquare(NIS_avg, NIS_exp)
+    
+    timesteps = np.arange(0,k)
+    fig, ax = plt.subplot()
+    fig.suptitle('NIS Testing')
+    ax.plot(timesteps, NIS_avg, color='b')
+    ax.plot(timesteps, r1, color="g")
+    ax.plot(timesteps, r2, color="g")
+    ax.set_title('NIS Estimation Results')
+    ax.set_xlabel('Timesteps, k')
+    ax.set_ylabel('NIS Statistic')
+
+    plt.tight_layout()
+    plt.show()
+    plt.savefig("NEES Testing.png")
+    
+    return NIS_avg, res
